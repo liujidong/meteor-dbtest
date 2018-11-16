509561b731559031df4db1e0da517bd675ab1cb7
liujidong <wwwliujidong@163.com>
2018-11-16 14:38:00 +0800

    users 示例 修改完善

--- client/useritem.js
+++ client/useritem.js
@@ -1,35 +1,16 @@
 import {Users} from '../lib/collection/Users.js';
 Template.useritem.helpers({
-	isEditing	: function(){
-		//从session中取得editid值
-		//到命令行中添加Session支持
-		//meteor add session
-		var eid = Session.get('editid');
-		//console.log('eid:'+eid);
-		//console.log('this_id:'+this._id);
-		//返回对比结果
-		var flag = this._id+''==eid;
-		return flag;
-	} 
+
 });
 Template.useritem.events({
 	'click .edit':function(e,tpl){
 		e.preventDefault();
 		//取得当前用户的_id
 		var id=this._id;
-		console.log('id:'+id);
+		//console.log('id:'+id);
 		//设置到session
 		Session.set('editid',id);
-	},
-	'click button':function(e,tpl){
-		var name=tpl.$("#username").val();
-		var age=tpl.$("#age").val();
-
-		var id=this._id;
-
-		Users.update({_id:id},{name:name,age:age},function(){
-			Session.set("editid",-1);
-		});
+		//return Users.findOne({editid:eid});
 	},
 	'click .remove':function(e,tpl){
 		if(confirm('确定删除？')){
--- client/users.js
+++ client/users.js
@@ -4,5 +4,35 @@ Template.users.helpers({
 		//调用数据库对象，执行查询
 		//find()返回json数据
 		return Users.find();
+	},
+	user:function(){
+		var eid = Session.get('editid');
+		//console.log('eid:'+eid);
+		if(eid != -1){
+			var user = Users.findOne({_id:eid});
+			//console.log(user);
+			return user;
+		}
+		return null;
+	}	
+});
+Template.users.events({
+	'click button':function(e,tpl){
+		var name=tpl.$("#username").val();
+		var age=tpl.$("#age").val();
+		var user = {name:name,age:age};
+		//var id=this._id;//此处不能用，特指该条数据的id
+		var id = Session.get('editid');
+		console.log(id);
+		if(id==false || id < 0){
+			user._id=Users.insert(user);
+			//Session.set("editid",-2);
+		}else{
+			user._id=id;
+			console.log(user);
+			Users.update({_id:id},user,function(){
+				Session.set("editid",-1);
+			});
+		}
 	}
 });
